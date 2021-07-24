a9760adcdea230cba5f9dbab0a5d0fe608607597
Christian Yao Komenan <kycdev@gmail.com>
2021-07-24 19:26:01 +0000

    KwordZ Client Angular publishing 202107241926

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const SERVER_URI = 'http://devkoms.com:9001/api/';
+const SERVER_URI = 'https://kwordz-api.devkoms.com/api/';
 const APIs_LOCATION = SERVER_URI+'open/file/';
 
 export interface File {
